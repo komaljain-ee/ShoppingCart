462643c961dd9c526e0ed7414cd8f327fa391e78
Pradip Hudekar <phudekar@equalexperts.com>
2016-07-14 11:13:22 +0530

    added counter

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './helloWorld.jsx';
 
-ReactDOM.render(<HelloWorld/>, document.getElementById("app"));
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      initialCount:  0
+    };
+  }
+
+  updateInitialCount(value){
+    let number = parseInt(value) || 0;
+    this.setState({initialCount: number});
+  }
+
+  render(){
+    return (
+      <div>
+        <input type="number" onChange={(e)=> {
+            this.updateInitialCount(e.target.value);
+          } }/>
+        <HelloWorld initialCount={this.state.initialCount}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>,
+    document.getElementById("app"));
--- public/js/helloWorld.jsx
+++ public/js/helloWorld.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-var HelloWorld = React.createClass({
-    render: function() {
-        return (
-            <div className="hello-world">Hello World!!!</div>
-        );
+class HelloWorld extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          count: this.props.initialCount || 0
+        };
+        console.log("component created");
     }
-});
+
+    componentDidMount(){
+      console.log("component mounted");
+    }
+
+    componentWillReceiveProps(newProps){
+      console.log("component going to receive props");
+      this.setState({count: newProps.initialCount});
+    }
+
+    componentDidReceiveProps(newProps){
+      console.log("component received props");
+    }
+
+    componentWillUpdate(newProps, newState){
+      console.log("component will update");
+    }
+
+    componentDidUpdate(newProps, newState){
+      console.log("component did update");
+    }
+
+    shouldComponentUpdate(newProps, newState){
+      console.log("should update");
+      return newProps.initialCount >= 0;
+    }
+
+    increment(){
+      this.setState({count: this.state.count + 1});
+    }
+
+    render(){
+        return (
+          <div>
+            <input type="button" value="Increment" onClick={()=>{this.increment()}}/>
+            <div className="hello-world">{this.state.count}</div>
+          </div>
+      );
+    }
+}
 
 module.exports = HelloWorld;
